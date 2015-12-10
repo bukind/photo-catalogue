d550a6b7ef9e87872fd2b986b49fee9fdf26be0b
Dmitry Bukin <bukin.dima@gmail.com>
2015-12-10 23:05:04 +1100

    Options

--- phcat.py
+++ phcat.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import argparse
 
 
 class Opts(object):
@@ -10,7 +11,34 @@ class Opts(object):
     """
 
     def __init__(self):
+        print sys.argv
+        parser = OptionParser()
+        parser.add_option("-r", "--root", help="root directory of photo catalogue")
+        parser.add_option("-s", "--scan", help="scan only this subdirectory")
+        (self._opts, self._args) = parser.parse_args()
+
+        _path = lambda x : os.path.realpath(os.path.abspath(x))
+
+        if self._opts.root is None:
+            raise ValueError, "root value is required"
+        if self._opts.scan is None:
+            self._opts.scan = self._opts.root
+
+        self._opts.root = _path(self._opts.root)
+        self._opts.scan = _path(self._opts.scan)
+
+        if not os.path.isdir(self._opts.root):
+            raise ValueError, "root must be existing directory"            
+        if not os.path.isdir(self._opts.scan):
+            raise ValueError, "scan must be existing directory"
+
+        if not self._opts.scan.startswith(self._opts.root):
+            raise ValueError, "scan must be a subdirectory of root"
+            
         return
+
+    def __str__(self):
+        return "opts=%s args=%s" % (self._opts,self._args)
 
     pass
 
@@ -20,6 +48,7 @@ class PhCat(object):
     """
 
     def __init__(self, opts):
+        print opts
         return
 
     def scan(self):
@@ -29,7 +58,7 @@ class PhCat(object):
 
 
 if __name__ == '__main__':
-    opts = Opts(sys.argv)
+    opts = Opts()
     cat = PhCat(opts)
     cat.scan()
     sys.exit(0)
